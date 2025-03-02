5cfd3a6f9cdf945c6e27de479dce395e5570bd07
Anooja M <anoojam@jabirs-MacBook-Air.local>
2025-03-02 18:03:16 +0530

    env changes

--- src/config/s3.js
+++ src/config/s3.js
@@ -2,11 +2,11 @@ import { S3Client } from "@aws-sdk/client-s3";
 import dotenv from "dotenv";
 
 dotenv.config();
-console.log(process.env.AWS_ACCESS_KEY, process.env.AWS_SECRET_ACCESS_KEY, process.env.AWS_REGION)
+
 const s3 = new S3Client({
     region: process.env.AWS_REGION,
     credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY,
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
     }
 });
